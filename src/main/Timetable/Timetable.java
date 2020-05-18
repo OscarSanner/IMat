323fc7b38809f056f1d95885f101054ab5aba9db
Puh00 <alex.phu@live.com>
2020-05-19 00:01:05 +0200

    Added 'Date' logic

--- src/main/Timetable/Timetable.java
+++ src/main/Timetable/Timetable.java
@@ -2,13 +2,18 @@ package main.Timetable;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import main.IWizardPage;
 import main.Payment.Payment;
 import main.PersonalData.PersonalData;
 
 import java.io.IOException;
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.lang.System.out;
 
 public class Timetable extends AnchorPane implements IWizardPage {
 
@@ -16,12 +21,20 @@ public class Timetable extends AnchorPane implements IWizardPage {
 
     //TODO Alla wizardklasser behöver ha en fungerande "Customer Support" knapp
 
-
     @FXML
     public AnchorPane timetableMainAnchorPane;
     public Payment paymentPage;
     public PersonalData parentBackendController;
     private Date deliveryTime;
+
+    //AnchorPanes elements
+    @FXML private Label date1Label;
+    @FXML private Label date2Label;
+    @FXML private Label date3Label;
+    @FXML private Label date4Label;
+    @FXML private Label date5Label;
+    @FXML private Label date6Label;
+    @FXML private Label date7Label;
 
 
     public Timetable(PersonalData parentBackendController){
@@ -32,9 +45,11 @@ public class Timetable extends AnchorPane implements IWizardPage {
 
         try {
             fxmlLoader.load();
+
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+        initDateLabels();
 
     }
 
@@ -55,4 +70,39 @@ public class Timetable extends AnchorPane implements IWizardPage {
     }
 
     //----------------FAKTISK KOD-----------------
+
+    public void initDateLabels(){
+        List<String> dates = getCurrentWeek();
+
+        List<Label> STUPIDLIST = new ArrayList<>(Arrays.asList(date1Label, date2Label, date3Label, date4Label, date5Label, date6Label, date7Label));
+        int count = 0;
+        for(Label label: STUPIDLIST){
+            label.setText(dates.get(count));
+            count++;
+        }
+
+    }
+    public List<String> getCurrentWeek(){    //Collects the dates for the current week.
+        List<String> dates = new ArrayList<>();
+        Date currentDate = new Date();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("dd-MM-yy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+
+        for(int i = 0; i<=7; i++){
+            dates.add(convertDateToReadable(c.getTime(), timeFormat));
+            c.add(Calendar.DATE, 1); //Incrementing date by one
+        }
+
+        return dates;
+    }
+    public String convertDateToReadable(Date date, SimpleDateFormat timeFormat){//Converts "dd--MM--yy" to "dd/MM"
+        StringBuilder convertedDate = new StringBuilder();
+
+        convertedDate.append((timeFormat.format(date)).substring(0,2)).append("/").append((timeFormat.format(date)).substring(3,5));
+        String tempDate = convertedDate.toString();
+        return tempDate;
+    }
+
+
 }
--- src/main/TimetableItem/TimetableItem.java
+++ src/main/TimetableItem/TimetableItem.java
@@ -1,17 +1,23 @@
 package main.TimetableItem;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class TimetableItem extends AnchorPane {
 
-    public TimetableItem(){
+
+    @FXML private Date date;
+
+    public TimetableItem(Date date){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TimetableItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
+        this.date = date;
 
         try {
             fxmlLoader.load();
